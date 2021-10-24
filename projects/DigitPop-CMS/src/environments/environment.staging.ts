f10aaa237e7bc375e188bcf1112896b3f549d6e4
BoggsSystems <jeff_boggs@hotmail.com>
2021-10-23 22:46:53 -0400

    Chane env variables

--- projects/DigitPop-CMS/src/environments/environment.staging.ts
+++ projects/DigitPop-CMS/src/environments/environment.staging.ts
@@ -1,8 +1,8 @@
 // Staging Environment
 export const environment = {
     production: true,
-    apiUrl:"https://digitpop-server-staging.herokuapp.com",
-    playerUrl:"https://digitpop-player-staging.herokuapp.com",
+    apiUrl:"https://second-digitpop-server-staging.herokuapp.com",
+    playerUrl:"https://second-digitpop-player-staging.herokuapp.com",
     cloudinaryUrl:"https://api.cloudinary.com/v1_1/",
     billsbyUrl:"https://public.billsby.com/api/v1/rest/core/digitpop",
     billsbyKey:"digitpop_f1efba4792104a3ab66c1cb59f43993a",
