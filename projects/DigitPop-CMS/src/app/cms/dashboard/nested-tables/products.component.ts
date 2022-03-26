84353392590dd95733420e887ed70c0a949ad1a4
= <=>
2022-03-26 00:49:09 +0000

    Add void to solve expand issue

--- projects/DigitPop-CMS/src/app/cms/dashboard/nested-tables/products.component.ts
+++ projects/DigitPop-CMS/src/app/cms/dashboard/nested-tables/products.component.ts
@@ -18,10 +18,13 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
             ),
         ]),
         trigger('pgDetailExpand', [
-            state('collapsed', style({ height: '0px', minHeight: '0' })),
+            state('collapsed, void', style({ height: '0px', minHeight: '0' })),
             state('expanded', style({ height: '*' })),
             transition(
                 'expanded <=> collapsed',
+                animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')
+            ), transition(
+                'expanded <=> void',
                 animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')
             ),
         ]),
