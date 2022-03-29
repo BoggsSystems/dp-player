c3e1598b14bfb32fc5fd0ccf97d9c0594a2fd735
= <=>
2022-03-29 17:51:02 +0000

    Responsive nav iterates and adds data-nav sections

--- projects/DigitPop-CMS/src/app/app.component.ts
+++ projects/DigitPop-CMS/src/app/app.component.ts
@@ -11,132 +11,182 @@ import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
 import { SignupComponent } from './xchane/signup/signup.component';
 @Component({
-  selector: 'DigitPop-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'DigitPop-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  isHandset$: Observable<boolean> = this.breakpointObserver
-    .observe(Breakpoints.Handset)
-    .pipe(
-      map((result) => result.matches),
-      shareReplay()
-    );
+    isHandset$: Observable<boolean> = this.breakpointObserver
+        .observe(Breakpoints.Handset)
+        .pipe(
+            map((result) => result.matches),
+            shareReplay()
+        );
 
-  email: any;
-  password: any;
-  dialogRef: any;
-  // currentUser: User;
-  showSpinner: boolean;
-  currentUser:any;
-  currentRole:any;
-  isLogin: any;
-  isCMS: any;
-  @ViewChild(HomeComponent) child:HomeComponent;
-  constructor(
-    public spinnerService: SpinnerService,
-    private breakpointObserver: BreakpointObserver,
-    public dialog: MatDialog,
-    private router: Router
-  ) {
-    // this.authenticationService.currentUser.subscribe(
-    //   (x) => (this.currentUser = x)
-    // );
-  }
-  ngOnInit(){
-    // this.currentRole= localStorage.getItem("currentrole");
-    // if(this.currentRole=="admin"||this.currentRole=="Business"){
-    //   this.router.navigate(['/cms/dashboard']);
-    // };
-    // if(this.currentRole=="consumer"){
-    //   this.router.navigate(['/xchane/dashboard']);
-    // };
-
-    if(localStorage.getItem("currentrole")){
-      localStorage.removeItem("currentrole");
+    email: any;
+    password: any;
+    dialogRef: any;
+    // currentUser: User;
+    showSpinner: boolean;
+    currentUser: any;
+    currentRole: any;
+    isLogin: any;
+    isCMS: any;
+    navSections: Object;
+    isEligible: boolean;
+    sectionsKeys: any;
+    @ViewChild(HomeComponent) child: HomeComponent;
+    constructor(
+        public spinnerService: SpinnerService,
+        private breakpointObserver: BreakpointObserver,
+        public dialog: MatDialog,
+        private router: Router
+    ) {
+        // this.authenticationService.currentUser.subscribe(
+        //   (x) => (this.currentUser = x)
+        // );
+        router.events.subscribe(() => {
+            this.getSections();
+        });
     }
-  }
-  ngDoCheck(){
-    this.isLogin = false
-    this.isCMS = false
-    this.currentUser= localStorage.getItem("currentuser");
-    this.currentRole= localStorage.getItem("currentrole");
-    
-    if (this.currentRole) {
-      this.isLogin = true
-    } 
-    if (this.currentRole == "Business") {
-      this.isCMS = true
+    ngOnInit() {
+        // this.currentRole= localStorage.getItem("currentrole");
+        // if(this.currentRole=="admin"||this.currentRole=="Business"){
+        //   this.router.navigate(['/cms/dashboard']);
+        // };
+        // if(this.currentRole=="consumer"){
+        //   this.router.navigate(['/xchane/dashboard']);
+        // };
+
+        if (localStorage.getItem("currentrole")) {
+            localStorage.removeItem("currentrole");
+        }
+    }
+    ngDoCheck() {
+        this.isLogin = false
+        this.isCMS = false
+        this.currentUser = localStorage.getItem("currentuser");
+        this.currentRole = localStorage.getItem("currentrole");
+
+        if (this.currentRole) {
+            this.isLogin = true
+        }
+        if (this.currentRole == "Business") {
+            this.isCMS = true
+        }
+
+        // if(this.currentRole=="admin"||this.currentRole=="Business"){
+        //   this.router.navigate(['/cms/dashboard']);
+        // };
+        // if(this.currentRole=="consumer"){
+        //   this.router.navigate(['/xchane/dashboard']);
+        // };
+        // console.log(this.currentUser);
+
+    }
+    openLoginDialog(): void {
+
+        const dialogRef = this.dialog.open(LoginComponent, {
+            width: '250px', height: "350px"
+        });
+
+        dialogRef.afterClosed().subscribe(() => {
+            console.log('The dialog was closed');
+        });
+
+    }
+    openSignup(): void {
+
+        const dialogRef = this.dialog.open(SignupComponent, {
+            width: '40%',
+            height: '70%'
+        });
+
+        dialogRef.afterClosed().subscribe(() => {
+            console.log('The dialog was closed')
+        });
+    }
+    openLogout(): void {
+        const dialogRef = this.dialog.open(LogoutComponent, {
+            width: '400px',
+            height: '150px'
+        });
+        dialogRef.afterClosed().subscribe(() => {
+            console.log('The dialog was closed');
+        });
+        // localStorage.removeItem("currentuser");
+        // localStorage.removeItem("currentrole");
+        // this.router.navigate(['/']);
     }
 
-    // if(this.currentRole=="admin"||this.currentRole=="Business"){
-    //   this.router.navigate(['/cms/dashboard']);
-    // };
-    // if(this.currentRole=="consumer"){
-    //   this.router.navigate(['/xchane/dashboard']);
-    // };
-    // console.log(this.currentUser);
+    openDialog(): void {
+        console.log("WZZZAAA");
+        this.router.navigate(['/xchane/landing']);
 
-  }
-  openLoginDialog(): void {
+        // const dialogRef = this.dialog.open(LoginComponent, {
+        //   width: '250px',
+        // });
 
-    const dialogRef = this.dialog.open(LoginComponent, {
-      width: '250px', height: "350px"
-    });
+        // dialogRef.afterClosed().subscribe(() => {
+        //   console.log('The dialog was closed');
+        // });
+    }
 
-    dialogRef.afterClosed().subscribe(() => {
-      console.log('The dialog was closed');
-    });
+    ngAfterContentInit(): void {
+        // if (this.authenticationService.currentUserValue) {
+        //   this.router.navigate(['/dashboard']);
+        // } else {
+        //   this.router.navigate(['/home']);
+        // }
+    }
 
-  }
-  openSignup(): void {
+    account() {
+        //this.router.navigate(['/account']);
 
-    const dialogRef = this.dialog.open(SignupComponent, {
-      width: '40%',
-      height: '70%'
-    });
-   
-    dialogRef.afterClosed().subscribe(() => {
-      console.log('The dialog was closed')
-    });
-  }
-  openLogout():void{
-    const dialogRef = this.dialog.open(LogoutComponent, {
-      width: '400px',
-      height: '150px'
-    });
-    dialogRef.afterClosed().subscribe(() => {
-      console.log('The dialog was closed');
-    });
-    // localStorage.removeItem("currentuser");
-    // localStorage.removeItem("currentrole");
-    // this.router.navigate(['/']);
-  }
+        this.router.navigate(['/xchane/landing']);
+    }
 
-  openDialog(): void {
-    console.log("WZZZAAA");
-    this.router.navigate(['/xchane/landing']);
+    getSections() {
+        if (!this.checkEligibility()) {
+            return null;
+        }
+        let sectionsObject = {};
+        const pageSections = document.querySelectorAll('[data-nav]');
 
-    // const dialogRef = this.dialog.open(LoginComponent, {
-    //   width: '250px',
-    // });
+        pageSections.forEach((section: Element) => {
+            let id = this.getSectionId(section),
+                title = this.getSectionTitle(section);
+            if (!(id in pageSections)) {
+                Object.assign(sectionsObject, { [id]: title })
+            }
+        });
+        this.navSections = sectionsObject;
+        this.sectionsKeys(sectionsObject);
+        return sectionsObject;
+    }
 
-    // dialogRef.afterClosed().subscribe(() => {
-    //   console.log('The dialog was closed');
-    // });
-  }
+    checkEligibility() {
+        const attributeExist = document.querySelector('[data-nav]') ?? false;
+        this.isEligible = attributeExist ? true : false;
+        return attributeExist;
+    }
 
-  ngAfterContentInit(): void {
-    // if (this.authenticationService.currentUserValue) {
-    //   this.router.navigate(['/dashboard']);
-    // } else {
-    //   this.router.navigate(['/home']);
-    // }
-  }
+    getSectionTitle(section: Element) {
+        const title = section.getAttribute('data-nav');
+        return title;
+    }
 
-  account() {
-    //this.router.navigate(['/account']);
+    getSectionId(section: Element) {
+        const id = section.getAttribute('id') ?? '';
+        return id;
+    }
 
-    this.router.navigate(['/xchane/landing']);
-  }
+    setSectionsKeys(sectionsObject: Object) {
+        this.sectionsKeys = Object.keys(sectionsObject);
+    }
+
+    scrollToSection(sectionId: string): void {
+        let targetSection = document.querySelector('#' + sectionId);
+        targetSection.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+    }
 }
