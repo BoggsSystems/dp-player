c4bd27a5c7ec732a8d5732d435176687b6f7bf5e
= <=>
2022-06-17 01:56:50 +0000

    Setup new Heroku apps links

--- projects/DigitPop-CMS/src/environments/environment.staging.ts
+++ projects/DigitPop-CMS/src/environments/environment.staging.ts
@@ -1,8 +1,8 @@
 // Staging Environment
 export const environment = {
     production: false,
-    apiUrl:"https://digitpop-staging-server-cli.herokuapp.com",
-    playerUrl:"https://digitpop-staging-player-cli.herokuapp.com",
+    apiUrl:"https://staging-digitpop-server.herokuapp.com",
+    playerUrl:"https://staging-digitpop-player.herokuapp.com",
     cloudinaryUrl:"https://api.cloudinary.com/v1_1/",
     billsbyUrl:"https://public.billsby.com/api/v1/rest/core/digitpop",
     billsbyKey:"digitpop_f1efba4792104a3ab66c1cb59f43993a",
