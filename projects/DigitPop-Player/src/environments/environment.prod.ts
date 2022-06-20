3a6fe00db794f1bca732a3e88dc4aad4714919de
BoggsSystems <jeff_boggs@hotmail.com>
2022-06-20 13:41:22 -0400

    Update homeUrl in config

--- projects/DigitPop-Player/src/environments/environment.prod.ts
+++ projects/DigitPop-Player/src/environments/environment.prod.ts
@@ -6,5 +6,5 @@ export const environment = {
   CLOUDINARY_UPLOAD_PRESET:"yspatu75",
   billsbyUrl:"https://public.billsby.com/api/v1/rest/core/digitpop",
   billsbyKey:"digitpop_f1efba4792104a3ab66c1cb59f43993a",
-  homeUrl:"https://18.195.23.143.herokuapp.com"
+  homeUrl:"https://production-digit-cms.herokuapp.com/"
 };
