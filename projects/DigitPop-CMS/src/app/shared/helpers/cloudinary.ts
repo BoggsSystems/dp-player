bfaff6190042fd06316d99a4931bb933bbc7c146
Mohamed Abdelkader <madsaaeq@gmail.com>
2022-12-05 16:55:23 +0200

    Resize video thumbnail

--- projects/DigitPop-CMS/src/app/shared/helpers/cloudinary.ts
+++ projects/DigitPop-CMS/src/app/shared/helpers/cloudinary.ts
@@ -1,17 +1,16 @@
 import {Project} from '../models/project';
+import {ProjectMedia} from '../models/ProjectMedia';
 
 export const Cloudinary = {
-  resizeThumbnail: (projects: Project[]) => {
-    projects.forEach((project: Project) => {
+  resizeThumbnail: (projects: Project[] | ProjectMedia[], imageWidth: number = 50, imageHeight: number = 50, objectFit: string = 'c_fill') => {
+    projects.forEach((project: Project | ProjectMedia) => {
       if ('thumbnail' in project) {
         const url = project.thumbnail.url;
         const queryTerm = 'upload/';
         const queryLength = queryTerm.length;
         const uploadsIndex = url.indexOf(queryTerm);
         const paramsIndex = uploadsIndex + queryLength;
-        const imgHeight = 50;
-        const imgWidth = 50;
-        const params = `c_fill,h_${imgHeight},w_${imgWidth}/`;
+        const params = `${objectFit},h_${imageHeight},w_${imageWidth}/`;
         project.thumbnail.url = url.substr(0, paramsIndex) + params + url.substr(paramsIndex);
       }
     });
--- projects/DigitPop-CMS/src/app/shared/services/videos-grid.service.ts
+++ projects/DigitPop-CMS/src/app/shared/services/videos-grid.service.ts
@@ -3,6 +3,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {map, shareReplay} from 'rxjs/operators';
+import {Cloudinary} from '../helpers/cloudinary';
 import {ProjectMedia} from '../models/ProjectMedia';
 import {Category} from '../models/category';
 
@@ -25,6 +26,7 @@ export class VideosGridService {
       params = categories.length ? params.append('categories', categories.join(',')) : null;
       this.cachedVideo$ = this.httpClient.get(this.endpoint, {params})
         .pipe(map((response: any) => {
+          response = Cloudinary.resizeThumbnail(response, 353, 170);
           return this.secondsToMMSS(response);
         }), shareReplay(1));
       this.cacheMap.set(categories.toString(), this.cachedVideo$);
