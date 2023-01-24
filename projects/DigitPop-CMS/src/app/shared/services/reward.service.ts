486f13f688de6887272404e61067f6526889fb77
Mohamed Abdelkader <madsaaeq@gmail.com>
2023-01-24 09:48:38 +0200

    Add caching to the rewards service

--- projects/DigitPop-CMS/src/app/shared/services/reward.service.ts
+++ projects/DigitPop-CMS/src/app/shared/services/reward.service.ts
@@ -2,6 +2,9 @@ import {Inject, Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {HTTP_XCHANE_AUTH} from '../../app.module';
 import {environment} from '../../../environments/environment';
+import {Observable} from 'rxjs';
+import {Reward} from '../models/reward';
+import {map, shareReplay} from 'rxjs/operators';
 
 
 const httpOptions = {
@@ -11,10 +14,19 @@ const httpOptions = {
 @Injectable({providedIn: 'root'})
 
 export class RewardService {
+  readonly endpoint = `${environment.apiUrl}/api/rewards`;
+  cachedRewards$: Observable<Reward[]>;
+
   constructor(@Inject(HTTP_XCHANE_AUTH) private http: HttpClient) {
   }
 
-  getRewards() {
-    return this.http.get(`${environment.apiUrl}/api/rewards/`);
+  getRewards = () => {
+    if (!this.cachedRewards$) {
+      this.cachedRewards$ = this.http.get(this.endpoint)
+        .pipe(map((response: any) => {
+          return response;
+        }), shareReplay(1));
+    }
+    return this.cachedRewards$;
   }
 }
