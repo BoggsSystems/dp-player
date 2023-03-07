864b4eda88d216eba477132b1af92b34237fcaa1
BoggsSystems <jeff_boggs@hotmail.com>
2023-03-06 23:58:44 -0500

    Change config urls to add www prefix

--- projects/DigitPop-Player/src/environments/environment.ts
+++ projects/DigitPop-Player/src/environments/environment.ts
@@ -6,6 +6,6 @@ export const environment = {
   CLOUDINARY_UPLOAD_PRESET: 'yspatu75',
   billsbyUrl: 'https://public.billsby.com/api/v1/rest/core/digitpop',
   billsbyKey: 'digitpop_f1efba4792104a3ab66c1cb59f43993a',
-  homeUrl: 'https://digitpop.com',
-  iOSFallbackUrl: 'https://digitpop.com'
+  homeUrl: 'https://www.digitpop.com',
+  iOSFallbackUrl: 'https://www.digitpop.com'
 };
