2bcd64d5250495899dd42faa46d93a915005cce1
Mohamed Abdelkader <madsaaeq@gmail.com>
2023-03-08 23:56:43 +0200

    Change iOSFallback URL to http

--- projects/DigitPop-Player/src/environments/environment.ts
+++ projects/DigitPop-Player/src/environments/environment.ts
@@ -7,5 +7,5 @@ export const environment = {
   billsbyUrl: 'https://public.billsby.com/api/v1/rest/core/digitpop',
   billsbyKey: 'digitpop_f1efba4792104a3ab66c1cb59f43993a',
   homeUrl: 'https://digitpop.com',
-  iOSFallbackUrl: 'https://www.digitpop.com'
+  iOSFallbackUrl: 'http://digitpop.com'
 };
