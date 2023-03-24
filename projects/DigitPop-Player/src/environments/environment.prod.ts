37583be3a9b33fb92c12c8c3c10b409b111178fd
Mohamed Abdelkader <madsaaeq@gmail.com>
2023-03-24 15:12:04 +0200

    Add websocket url to prod env

--- projects/DigitPop-Player/src/environments/environment.prod.ts
+++ projects/DigitPop-Player/src/environments/environment.prod.ts
@@ -7,5 +7,6 @@ export const environment = {
   billsbyUrl: 'https://public.billsby.com/api/v1/rest/core/digitpop',
   billsbyKey: 'digitpop_f1efba4792104a3ab66c1cb59f43993a',
   homeUrl: 'https://www.digitpop.com',
-  iOSFallbackUrl: 'http://digitpop.com'
+  iOSFallbackUrl: 'http://digitpop.com',
+  websocketURL: 'wss://production-digitpop-server.herokuapp.com'
 };
