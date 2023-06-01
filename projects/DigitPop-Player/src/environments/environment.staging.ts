33094a9eab372d92be3257b0b17d53f44e064ae7
Mohamed Abdelkader <madsaaeq@gmail.com>
2023-06-01 04:10:18 +0300

    Change cofiguration to staging

--- projects/DigitPop-Player/src/environments/environment.staging.ts
+++ projects/DigitPop-Player/src/environments/environment.staging.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://digitpop-server-staging.herokuapp.com',
+  apiUrl: 'https://dp-staging-server.herokuapp.com/',
   cloudinaryUrl: 'https://api.cloudinary.com/v1_1/',
   CLOUDINARY_CLOUD_NAME: 'boggssystems',
   CLOUDINARY_UPLOAD_PRESET: 'yspatu75',
   billsbyUrl: 'https://public.billsby.com/api/v1/rest/core/stagingdigitpop',
   billsbyKey: 'stagingdigitpop_e63c7ed880354970adf026d1a9c80e97',
   homeUrl: 'https://digitpop.com',
-  iOSFallbackUrl: 'https://digitpop.com'
+  iOSFallbackUrl: 'https://digitpop.com',
+  websocketURL: 'wss://dp-staging-server.herokuapp.com/'
 };
